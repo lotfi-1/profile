390f6985e2ac7a302c89afef6247a7c8a6ec3b88
lotfi <lotfi-1>
2024-06-17 23:12:22 +0100

    initial commit

--- app/api/expertise/route.ts
+++ app/api/expertise/route.ts
@@ -4,7 +4,7 @@ const expertise = [
   {
     id: 0,
     title: "Full Stack Development",
-    desc: "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients.",
+    desc: "I am a skilled MERN stack developer with extensive experience in building robust web applications using postgreSql, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients.",
   },
   {
     id: 3,
@@ -28,6 +28,6 @@ export async function GET(request: Request) {
   //   },
   // })
   // const product = await res.json()
- 
-  return Response.json(expertise)
+
+  return Response.json(expertise);
 }
